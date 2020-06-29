a89097f7f9e9968bee55a8c451fa2d82ac437ec5
trishit_techie <developer.codewithtrishit@gmail.com>
2020-06-29 20:37:00 +0530

    final commit

---
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.notes;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> notes = new ArrayList<String>();
+    static  ArrayAdapter arrayAdapter;
+    SharedPreferences sharedPreferences ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        sharedPreferences =   getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>)sharedPreferences.getStringSet("notes",null);
+        if(set == null){
+            notes.add("Example note");
+        }
+        else{
+            notes = new ArrayList(set);
+        }
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+                intent.putExtra("noteID", position);
+                startActivity(intent);
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int itemToBeDeleted = position;
+               new AlertDialog.Builder(MainActivity.this)
+                       .setIcon(android.R.drawable.ic_delete)
+                       .setTitle("Delete note")
+                       .setMessage("Are you sure you want to delete this note?")
+                       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialog, int which) {
+                               notes.remove(itemToBeDeleted);
+                               arrayAdapter.notifyDataSetChanged();
+
+                               HashSet<String> set = new HashSet<>(MainActivity.notes);
+                               sharedPreferences.edit().putStringSet("notes", set).apply();
+                           }
+                       })
+                       .setNegativeButton("No", null)
+                       .show();
+
+                return true;
+            }
+        });
+    }
+    //MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+            startActivity(intent);
+        }
+        return false;
+    }
+}
